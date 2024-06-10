e3b66b0f6b1f8701a116a4ecb636b72dbbcf3a08
Oh Jiwon <73912947+Osongpodo@users.noreply.github.com>
2024-06-10 14:44:07 +0900

    Add_SetMap_Script

--- 2DmapTo3Dterrain/Assets/Scripts/SetMap.cs
+++ 2DmapTo3Dterrain/Assets/Scripts/SetMap.cs
@@ -8,37 +8,48 @@ using System.Globalization;
 public class SetMap : MonoBehaviour
 {
     public OnlineMaps map;
+    public OnlineMapsCameraOrbit camOrbit;
     public OnlineMapsBingMapsElevationManager elevationManager;
+    public OnlineMapsControlBase control;
 
     public TMP_InputField inputLong;
     public TMP_InputField inputLat;
 
-    public float lo;
     public float la;
-    public float zoom;
+    public float lo;
+
+    void Start()
+    {
+        camOrbit = map.GetComponent<OnlineMapsCameraOrbit>();
+        elevationManager = map.GetComponent<OnlineMapsBingMapsElevationManager>();
+        control = map.GetComponent<OnlineMapsControlBase>();
+    }
 
     void Update()
     {
-        lo = float.Parse(inputLong.text);
-        la = float.Parse(inputLat.text);
+
     }
 
     // 경도, 위도 입력받아 map 생성
     public void createMap()
     {
+        // 위치 받아오기
+        lo = float.Parse(inputLong.text);
+        la = float.Parse(inputLat.text);
+
         Debug.Log(lo + "," + la);
 
-        map.SetPosition(lo, la);
-        
-        //zoom이 필요한 경우
-        //map.SetPositionAndZoom(longitude, latitude, zoom);
+        // zoom 크기 17로 해당 위치의 지도 생성
+        map.SetPositionAndZoom(lo, la, 17);
+
+        // Elevation 설정
+        elevationManager.scale = 2f;
+
+        // 위치 고정 (좌클릭 이동 off)
+        control.allowUserControl = false;
+
+        // Top view 고정
+        camOrbit.lockTilt = true;
+        camOrbit.lockPan = true;
     }
-
-    // 좌클릭 이동 off
-
-    // zoom 고정
-
-    // zoom에 맞는 elevation 설정
-
-    // 편집 모드에서는 top view, 메인 모드에서는 고정 off
 }
